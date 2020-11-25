e6fedf0034787bc56b2a77d866af52b6aabc7edb
Jerry Jierui Yang <jerry@lnxsrv06.seas.ucla.edu>
2020-11-25 15:59:49 -0800

    second

--- randmain.mk
+++ randmain.mk
@@ -1,3 +1,12 @@
+
+#shared files
+randlibhw.so:
+        $(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
+        $(CC) $(CFLAGS) -shared -o randlibhw.so randlibhw.o
+randlibsw.so:
+        $(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
+        $(CC) $(CFLAGS) -shared -o randlibsw.so randlibsw.o
+
 #build main (3 ways)
 randmain:
 	#static
@@ -6,11 +15,3 @@ randmain:
 	$(CC) $(CFLAGS) -c randcpuid.c -o randcpuid.o
 	#dynamic post main
 	$(CC) $(CFLAGS) -ldl -Wl,-rpath=$(PWD) randmain.o randcpuid.o -o randmain
-
-#shared files
-randlibhw.so:
-	$(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
-	$(CC) $(CFLAGS) -shared -o randlibhw.so randlibhw.o
-randlibsw.so:
-	$(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
-	$(CC) $(CFLAGS) -shared	-o randlibsw.so randlibsw.o
