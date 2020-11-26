158fef762458d48c79947b80c648f6e0a812e51e
Jerry Jierui Yang <jerry@lnxsrv06.seas.ucla.edu>
2020-11-25 16:01:04 -0800

    wtf

--- randmain.mk
+++ randmain.mk
@@ -1,11 +1,10 @@
-
 #shared files
 randlibhw.so:
-        $(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
-        $(CC) $(CFLAGS) -shared -o randlibhw.so randlibhw.o
+	$(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
+	$(CC) $(CFLAGS) -shared -o randlibhw.so randlibhw.o
 randlibsw.so:
-        $(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
-        $(CC) $(CFLAGS) -shared -o randlibsw.so randlibsw.o
+	$(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
+	$(CC) $(CFLAGS) -shared -o randlibsw.so randlibsw.o
 
 #build main (3 ways)
 randmain:
